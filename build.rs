25b629945c8c908a5e7a5349e51ced2971ff2606
Sondre Lefsaker <sondrele@stud.ntnu.no>
2015-02-07 15:45:05 +0100

    Added run-function

--- build.rs
+++ build.rs
@@ -1,10 +1,21 @@
 #![feature(io)]
 
 use std::old_io::Command;
+use std::old_io::process::InheritFd;
+
+fn run(cmd: &mut Command) {
+    println!("running: {:?}", cmd);
+    let status = match cmd.stdout(InheritFd(1)).stderr(InheritFd(2)).status() {
+        Ok(status) => status,
+        Err(e) => panic!("failed to spawn process: {}", e),
+    };
+    if !status.success() {
+        panic!("nonzero exit status: {}", status);
+    }
+}
 
 fn main() {
-    match Command::new("./build.sh").status() {
-        Ok(s) => println!("{}", s),
-        Err(e) => println!("{}", e)
-    }
+    let mut fetch_src = Command::new("./build.sh");
+
+    run(&mut fetch_src);
 }
